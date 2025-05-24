0b004a82196c27010abb5d8ad120a9487956d8a4
sreenivas2025 <psrdevops575@gmail.com>
2025-05-24 15:57:40 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "welcome!";
+    private static final String MESSAGE = "Hello!";
 
     public App() {}
 
